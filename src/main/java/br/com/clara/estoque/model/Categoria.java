e2656696de6b6c8cb518210b5aab908a7e6efe17
clarinha2124 <clara.silva72@etec.sp.gov.br>
2024-05-28 09:40:24 -0300

    estoque

---
+++ src/main/java/br/com/clara/estoque/model/Categoria.java
@@ -0,0 +1,52 @@
+package br.com.clara.estoque.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name="categoria")
+public class Categoria {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomecategoria() {
+        return nomecategoria;
+    }
+
+    public void setNomecategoria(String nomecategoria) {
+        this.nomecategoria = nomecategoria;
+    }
+
+    @Id
+    private int id;
+            private  String nomecategoria;
+
+            @OneToMany(mappedBy = "categoria")
+            private List<Produto>produtoslista= new ArrayList<>();
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id == categoria.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
