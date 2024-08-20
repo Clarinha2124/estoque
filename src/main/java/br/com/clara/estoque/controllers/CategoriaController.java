48138432cf81d12d0f4fcdcaa7a2e16ae0f1719c
Clarinha2124 <clara.silva72@etec.sp.gov.br>
2024-08-20 12:24:47 -0300

    20/08

--- src/main/java/br/com/clara/estoque/controllers/CategoriaController.java
+++ src/main/java/br/com/clara/estoque/controllers/CategoriaController.java
@@ -1,14 +1,15 @@
 package br.com.clara.estoque.controllers;
 
 import br.com.clara.estoque.model.Categoria;
+import br.com.clara.estoque.repositories.CategoriaRepository;
 import br.com.clara.estoque.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
@@ -18,6 +19,13 @@ public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
 
+@Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping()
+    public List<Categoria>listarTodasCategorias(){
+        return categoriaRepository.findAll(Sort.by("nome").ascending());
+    }
 
     @PostMapping()
 
--- src/main/java/br/com/clara/estoque/controllers/ProdutoController.java
+++ src/main/java/br/com/clara/estoque/controllers/ProdutoController.java
@@ -1,23 +1,33 @@
 package br.com.clara.estoque.controllers;
 
+import br.com.clara.estoque.model.Categoria;
 import br.com.clara.estoque.model.Produto;
+import br.com.clara.estoque.repositories.ProdutoRepository;
 import br.com.clara.estoque.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/produtos")
+@RequestMapping("produtos")
 
 public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+
+    @GetMapping
+    public List<Produto> listarTodosProdutos(){
+        return produtoRepository.findAll(Sort.by("nome").ascending());
+    }
 
     @PostMapping()
     public ResponseEntity<Produto> inserir (@RequestBody Produto produto){
